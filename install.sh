a6cdf0cb7b25a5015f2e1f6aa2bbe8dbf81bae31
Nathan Hui <ntlhui@gmail.com>
2021-11-03 23:30:27 -0700

    Fixed runServer path

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DEBUG=0
 PYTHON=/usr/bin/python3
+RUN_SERVER=/usr/local/bin/
 
 if [ $EUID != 0 ]; then
     sudo "$0" "$@"
@@ -9,9 +10,10 @@ fi
 
 usage() {
     echo "${0} usage: "
-    echo "    -d          Specify debug"
-    echo "    -p python   Specify python interpreter"
-    echo "    -h          Display this message"
+    echo "    -d            Specify debug"
+    echo "    -p python     Specify python interpreter"
+    echo "    -r runServer  Specify runServer.py location"
+    echo "    -h            Display this message"
 }
 
 while getopts ":dp:" arg; do
@@ -21,6 +23,9 @@ while getopts ":dp:" arg; do
             ;;
         p)
             PYTHON=${OPTARG}
+            ;;
+        r)
+            RUN_SERVER=${OPTARG}
             ;;
         *)
             usage
@@ -33,7 +38,8 @@ if [[ ${DEBUG} -eq 1 ]]; then
 else
     ${PYTHON} -m pip install .
 fi
-PYTHONESC=$(echo ${PYTHON} | sed 's/\/\\\//g')
+PYTHONESC=$(echo ${PYTHON} | sed 's/\//\\\//g')
+RUN_SERVERESC=$(echo ${RUN_SERVER} | sed 's/\//\\\//g')
 if [[ ${DEBUG} -eq 0 ]]
 then
     
@@ -42,7 +48,7 @@ then
         cp sample-config.yaml /usr/local/etc/asm_config.yaml
     fi
     
-    cat asm_server.service | sed -e "s/python/${PYTHONESC}/g" > /lib/systemd/system/asm_server.service
+    cat asm_server.service | sed -e "s/python/${PYTHONESC}/g" | sed -e "s/runServer.py/${RUN_SERVERESC}/g" > /lib/systemd/system/asm_server.service
     chmod 644 /lib/systemd/system/asm_server.service
     systemctl daemon-reload
     systemctl enable asm_server.service
