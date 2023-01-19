db41f507c72228327da7ba62c41407ac7ce44a0a
Nathan Hui <nthui@eng.ucsd.edu>
2023-01-18 16:21:08 -0800

    Fixed dev environment and packaging

--- setup.py
+++ setup.py
@@ -1,16 +1,32 @@
-from distutils.core import setup
+'''ASM Data Server Setup file
+'''
+from setuptools import setup, find_packages
+
 import DataServer
-setup(name='ASM-DataServer',
-      version=DataServer.__version__,
-      description="Aye-Aye Sleep Monitoring Data Server",
-      author="UC San Diego Engineers for Exploration",
-      author_email="e4e@eng.ucsd.edu",
-      packages=['DataServer'],
-      scripts=['runServer.py'],
-      install_requires=[
-          'PyYAML',
-          'asm_protocol',
-          'appdirs',
-          'pytest'
-        ]
-      )
+
+setup(
+	name='ASM-DataServer',
+	version=DataServer.__version__,
+	description="Aye-Aye Sleep Monitoring Data Server",
+	author="UC San Diego Engineers for Exploration",
+	author_email="e4e@eng.ucsd.edu",
+	packages=find_packages(),
+	entry_points={
+		'console_scripts': [
+			'runServer = DataServer.runServer:main'
+		]
+	},
+	install_requires=[
+		'PyYAML',
+		'asm_protocol @ git+https://github.com/UCSD-E4E/ASM_protocol.git',
+		'appdirs',
+		'pytest'],
+	extras_require={
+		'dev': [
+			'pytest',
+			'coverage',
+			'pylint',
+			'wheel',
+		]
+	},
+)
